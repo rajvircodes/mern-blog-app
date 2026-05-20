ee69bfb345e01844042546f8d73c815855609a80
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-05-20 12:05:23 +0530

    feat(upload) post routes updated with upload routes

--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -9,11 +9,12 @@ const {
   deletePost,
 } = require("../controller/post.controller.js");
 const { protect } = require("../middleware/auth.middleware.js");
+const upload = require('../middleware/upload.middleware.js') 
 
 router.get("/", getAllPosts); // Public
 router.get("/:id", getPostById); // Public
-router.post("/", protect, createPost); // Protected
-router.put("/:id", protect, updatePost); // Protected
+router.post("/", protect, upload.single("coverImage"), createPost); // Protected
+router.put("/:id", protect,upload.single("coverImage"), updatePost); // Protected
 router.delete("/:id", protect, deletePost); // Protected
 
 module.exports = router;
